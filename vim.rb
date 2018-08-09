6d99ab5693b06cdb832704532496cd50d3e03320
Casey Boettcher <boettchc@oregonstate.edu>
2018-08-09 12:54:52 -0400

    Audited but untested formula

---
+++ vim.rb
@@ -0,0 +1,103 @@
+class Vim < Formula
+  desc "Vim without Perl, no GUI, and Python 3"
+  homepage "https://www.vim.org/"
+  url "https://github.com/vim/vim/archive/v8.1.0250.tar.gz" 
+  sha256 "f35193627469f4dc307e9286b21b2c818faaa2c67f03fb4983b58010f26f7f78"
+  head "https://github.com/vim/vim.git"
+
+  
+  deprecated_option "override-system-vi" => "with-override-system-vi"
+
+  option "with-override-system-vi", "Override system vi"
+  option "with-gettext", "Build vim with National Language Support (translated messages, keymaps)"
+  option "with-client-server", "Enable client/server mode"
+
+  depends_on "perl" => :optional
+  depends_on "ruby" => :optional
+  depends_on "python" => :optional
+  depends_on "gettext" => :optional
+  depends_on "lua" => :optional
+  depends_on "luajit" => :optional
+  depends_on :x11 if build.with? "client-server"
+  
+  conflicts_with "ex-vi",
+    :because => "vim and ex-vi both install bin/ex and bin/view"
+
+  def install
+    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
+
+    # https://github.com/Homebrew/homebrew-core/pull/1046
+    ENV.delete("SDKROOT")
+
+    # vim doesn't require any Python package, unset PYTHONPATH.
+    ENV.delete("PYTHONPATH")
+
+    opts = [ "--with-features=huge", "--enable-darwin", "--enable-gui=no",
+            "--without-x", "--enable-luainterp=yes", "--enable-python3interp=yes", 
+            "--enable-tclinterp=yes", "--enable-rubyinterp=yes", "--enable-perlinterp=no",
+            "--enable-largefile", "--enable-acl", "--with-mac-arch=intel", 
+            "--with-developer-dir=/Library/Developer" ]            
+
+    opts << "--disable-nls" if build.without? "gettext"
+
+      if build.with? "luajit"
+        opts << "--with-luajit"
+        opts << "--with-lua-prefix=#{Formula["luajit"].opt_prefix}"
+      else
+        opts << "--with-lua-prefix=#{Formula["lua"].opt_prefix}"
+      end
+
+      if build.with?("lua") && build.with?("luajit")
+        onoe <<~EOS
+          Vim will not link against both Luajit & Lua simultaneously.
+          Proceeding with Lua.
+        EOS
+        opts -= %w[--with-luajit]
+      end
+
+    # We specify HOMEBREW_PREFIX as the prefix to make vim look in the
+    # the right place (HOMEBREW_PREFIX/share/vim/{vimrc,vimfiles}) for
+    # system vimscript files. We specify the normal installation prefix
+    # when calling "make install".
+    # Homebrew will use the first suitable Perl & Ruby in your PATH if you
+    # build from source. Please don't attempt to hardcode either.
+    system "./configure", "--prefix=#{HOMEBREW_PREFIX}",
+                          "--mandir=#{man}",
+                          "--enable-multibyte",
+                          "--with-tlib=ncurses",
+                          "--enable-cscope",
+                          "--enable-terminal",
+                          "--with-compiledby='SocketBox <admin@socketbox.com>'",
+                          *opts
+    system "make"
+    # Parallel install could miss some symlinks
+    # https://github.com/vim/vim/issues/1031
+    ENV.deparallelize
+    # If stripping the binaries is enabled, vim will segfault with
+    # statically-linked interpreters like ruby
+    # https://github.com/vim/vim/issues/114
+    system "make", "install", "prefix=#{prefix}", "STRIP=#{which "true"}"
+    bin.install_symlink "vim" => "vi" if build.with? "override-system-vi"
+  end
+
+  test do
+    if build.with? "python@2"
+      (testpath/"commands.vim").write <<~EOS
+        :python import vim; vim.current.buffer[0] = 'hello world'
+        :wq
+      EOS
+      system bin/"vim", "-T", "dumb", "-s", "commands.vim", "test.txt"
+      assert_equal "hello world", File.read("test.txt").chomp
+    elsif build.with? "python"
+      (testpath/"commands.vim").write <<~EOS
+        :python3 import vim; vim.current.buffer[0] = 'hello python3'
+        :wq
+      EOS
+      system bin/"vim", "-T", "dumb", "-s", "commands.vim", "test.txt"
+      assert_equal "hello python3", File.read("test.txt").chomp
+    end
+    if build.with? "gettext"
+      assert_match "+gettext", shell_output("#{bin}/vim --version")
+    end
+end
+end
